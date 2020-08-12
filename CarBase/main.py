319444b4c8ceedf1b32b2d4d6f5c5fa93f63f9b9
tlorine <evolutiontv01@gmail.com>
2020-08-12 19:26:34 +0300

    Week 1 and Week 2 and Week 3

--- CarBase/main.py
+++
@@ -1,5 +0,0 @@
-import Cars
-
-car = Cars.CarBase("gavno", "gerpis/sperma.jpg", "gavno")
-truck = Cars.Truck("Mitshubishi", "sperma.jpg", "sad", "55.2x21.54x232")
-print(truck.body_width, truck.body_height, truck.body_length)
--- FileReader/main.py
+++
@@ -1,6 +0,0 @@
-import solution
-
-reader = solution.FileReader("readfile")
-
-print(reader.read())
-print(type(reader))
